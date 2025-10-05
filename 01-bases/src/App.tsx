c8c1cf890a590de9ee44b55c2a0ab05c46ea9d30
richie20502 <richie.lugo.recillas.1990@gmail.com>
2025-10-04 23:28:22 -0600

    Copunter Component

--- 01-bases/src/App.tsx
+++ 01-bases/src/App.tsx
@@ -2,15 +2,17 @@ import "./App.css";
 
 // import { BasicTypes } from "./typescript/BasicTypes";
 // import { ObjectLiterals } from "./typescript/
+import { Counter } from "./components/Counter";
 // import { BasicFunctions } from "./typescript/BasicFunctions";
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-svh">
+    <div className="flex flex-col justify-center items-center h-svh">
       <h1 className="text-4xl mb-5"> Vite + React</h1>
       {/* <BasicTypes /> */}
       {/* <ObjectLiterals /> */}
       {/* <BasicFunctions /> */}
+      <Counter />
     </div>
   );
 }
