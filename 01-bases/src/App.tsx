5911d78a1f29d40dc22a976d3f033cd0646bcff5
richie20502 <richie.lugo.recillas.1990@gmail.com>
2025-10-04 22:59:53 -0600

    agregaddo Tailwindcss

--- 01-bases/src/App.tsx
+++ 01-bases/src/App.tsx
@@ -2,16 +2,16 @@ import "./App.css";
 
 // import { BasicTypes } from "./typescript/BasicTypes";
 // import { ObjectLiterals } from "./typescript/
-import { BasicFunctions } from "./typescript/BasicFunctions";
+// import { BasicFunctions } from "./typescript/BasicFunctions";
 
 function App() {
   return (
-    <>
-      <h1>Vite + React</h1>
+    <div className="flex justify-center items-center h-svh">
+      <h1 className="text-4xl mb-5"> Vite + React</h1>
       {/* <BasicTypes /> */}
       {/* <ObjectLiterals /> */}
-      <BasicFunctions />
-    </>
+      {/* <BasicFunctions /> */}
+    </div>
   );
 }
 
